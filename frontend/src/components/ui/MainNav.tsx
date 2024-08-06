07adc387efb965b8ba26e0463d969b1387abc3fd
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2024-08-07 03:03:42 +0530

    Landing Page frontend portion was updated and added Footer

---
+++ frontend/src/components/ui/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold hover:text-orange-500 hover:bg-white"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;
--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,16 +1,20 @@
+import Footer from "@/components/ui/Footer";
 import Header from "@/components/ui/Header";
+import Hero from "@/components/ui/Hero";
 
 type Props = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
-const Layout = ({children}: Props) => {
-    return (
-        <div className = "flex flex-col min-h-screen">
-            <Header/>
-            <div className="container mx-auto flex-1 py-10">{children}</div>
-        </div>
-    )
-}
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Hero />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
