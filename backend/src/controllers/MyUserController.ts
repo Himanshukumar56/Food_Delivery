0df6aa281b42230321ca541f4dd432ca882fd5fc
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2024-08-11 00:28:17 +0530

    Started working on the backend, connecting data from auth server to localhost, to be displayed on user-profile page

---
+++ backend/src/controllers/MyUserController.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from "express";
+import User from "../models/user";
+
+const createCurrentUser = async (req: Request, res: Response) => {
+    try{
+        const {auth0Id} = req.body;
+        const exisitingUser = await User.findOne({auth0Id});
+        if(exisitingUser){
+            return res.status(200).send();
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message : "Error creating the user"});
+    }
+};
+
+export default {
+    createCurrentUser,
+}
