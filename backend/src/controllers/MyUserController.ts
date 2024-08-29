627674007865768a29679eec6f504e8da3dfe0de
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2024-08-29 15:40:18 +0530

    Started Working on User Profile Page

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,48 +1,46 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import User from "../models/user";
 
 const createCurrentUser = async (req: Request, res: Response) => {
-    try{
-        const {auth0Id} = req.body;
-        const exisitingUser = await User.findOne({auth0Id});
-        if(exisitingUser){
-            return res.status(200).send();
-        }
+  try {
+    const { auth0Id } = req.body;
+    const exisitingUser = await User.findOne({ auth0Id });
+    if (exisitingUser) {
+      return res.status(200).send();
+    }
 
-        const newUser = new User(req.body);
-        await newUser.save();
-        res.status(201).json(newUser.toObject());
-    }
-    catch(error){
-        console.log(error);
-        res.status(500).json({message : "Error creating the user"});
-    }
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error creating the user" });
+  }
 };
 
-const updateCurrentUser = async (req:Request, res:Response) => {
-    try {
-        const {name, addressLine1, country, city} = req.body;
-        const user = await User.findById(req.userId);
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
 
-        if(!user) {
-            return res.status(404).json({message : "User no found"});
-        }
-
-        user.name = name;
-        user.addressLine1 = addressLine1;
-        user.city = city;
-        user.country = country;
-
-        await user.save();
-        res.send(user);
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({message : "Error updating User"});
+    if (!user) {
+      return res.status(404).json({ message: "User no found" });
     }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
+
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating User" });
+  }
 };
 
 export default {
-    createCurrentUser,
-    updateCurrentUser,
-}
+  createCurrentUser,
+  updateCurrentUser,
+};
